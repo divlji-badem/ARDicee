5078615141469458c168e6f09ce83064d11664d2
Jelena Tasic <jelenatasicsd@gmail.com>
2021-11-08 09:08:27 +0100

    Roll dice click on a button or shake.

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -10,7 +10,7 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    var diceArray = [SCNNode]()
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
@@ -71,11 +71,45 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                         hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius,
                         hitResult.worldTransform.columns.3.z
                     )
+                    diceArray.append(diceNode)
                     sceneView.scene.rootNode.addChildNode(diceNode)
+                    roll(dice: diceNode)
                 }
             }
 
         }
+    }
+    
+
+    
+    func rollAll() {
+        if !diceArray .isEmpty {
+            for dice in diceArray {
+                roll(dice: dice)
+            }
+        }
+    }
+    
+    func roll(dice: SCNNode) {
+        let randomX = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
+        let randomZ = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
+        // * 5 to speed rotation
+        dice.runAction(
+            SCNAction.rotateBy(
+                x: CGFloat(randomX * 5),
+                y: 0, z: CGFloat(randomZ * 5),
+                duration: 0.5)
+        )
+    }
+    
+    // roll all dices on click on button
+    @IBAction func rollAgain(_ sender: UIBarButtonItem) {
+        rollAll()
+    }
+    
+    // roll on shake
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        rollAll()
     }
     
     // Detect horisontal surface and give width and height
